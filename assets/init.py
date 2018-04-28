392d0a82bb9820c6904e9926ab2939729177274a
bernardmaltais <bernard.maltais@canada.ca>
2018-04-27 23:55:33 -0400

    Update init.py

--- assets/init.py
+++ assets/init.py
@@ -226,15 +226,14 @@ class ServiceRun():
 
         self.__create_cluster(list_nodes, number_node)
         list_containers = list_nodes
-        print("Attempting to extend volumes to added node")
-        self.__extend_all_volumes(self.__list_volumes, number_node, self.__gluster_directory,list_nodes)
 
     # I create all volumes
     if self.__is_already_on_glusterfs() is True:
         list_nodes = list_containers.copy()
         list_nodes[current_container["name"]] = current_container
         self.__create_all_volumes(self.__list_volumes, self.__transport, self.__stripe, number_node, self.__quota,self.__gluster_directory,list_nodes)
-
+        print("Attempting to extend volumes to added node")
+        self.__extend_all_volumes(self.__list_volumes, number_node, self.__gluster_directory,list_nodes)
 
 
 
